d0fb3c978408c4dc535b68c7f82cb40559c9bcaf
anthonyminyungi <dbstnsdl12@naver.com>
2019-10-03 01:18:38 +0900

    Solving 11651,11650 (Additional Study about Sorting)
    Signed-off-by: anthonyminyungi <dbstnsdl12@naver.com>

---
+++ BOJ/JAVA/PointSort2_11651.java
@@ -0,0 +1,37 @@
+package com.java.BOJ.JAVA;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class PointSort2_11651 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            String[] a = br.readLine().split(" ");
+
+            arr[i][0] = Integer.parseInt(a[0]);
+            arr[i][1] = Integer.parseInt(a[1]);
+        }
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] x, int[] y) {
+                if (x[1] == y[1]) {
+                    return Integer.compare(x[0], y[0]);
+                }
+                return Integer.compare(x[1], y[1]);
+            }
+        });
+
+        for (int i = 0; i < N; i++) {
+            bw.append(arr[i][0] + " " + arr[i][1] + "\n");
+        }
+        bw.flush();
+    }
+
+}
