364deaccbbedbfd761761a46ac9dfacb77816642
anthonyminyungi <dbstnsdl12@naver.com>
2019-11-11 04:33:33 +0900

    solved 4705.
    Signed-off-by: anthonyminyungi <dbstnsdl12@naver.com>

---
+++ BOJ/JAVA/RoamingRomans_4705.java
@@ -0,0 +1,19 @@
+package com.java.BOJ.JAVA;
+
+import java.io.*;
+
+public class RoamingRomans_4705 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        double convert = 5280.0 / 4854.0;
+
+        String a;
+        while (!(a = br.readLine()).equals("0.0")) {
+            double in = Double.parseDouble(a);
+            bw.append(String.format("%.2f", in)).append(" English miles equals ").append(String.format("%.2f", in * convert)).append(" Roman miles or ").append(String.valueOf(Math.round((in * convert) * 1000))).append(" paces.\n");
+        }
+        bw.flush();
+
+    }
+}
