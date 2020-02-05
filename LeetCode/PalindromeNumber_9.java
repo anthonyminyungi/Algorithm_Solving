4b268c4583b080be6e729b5d26ffb08603f9226f
anthonymin <dbstnsdl12@naver.com>
2020-02-05 22:55:22 +0900

    Add two problems from leetcode already solved
    Signed-off-by: anthonymin <dbstnsdl12@naver.com>

---
+++ LeetCode/PalindromeNumber_9.java
@@ -0,0 +1,17 @@
+package com.java.LeetCode;
+
+public class PalindromeNumber_9 {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121));
+    }
+
+    public static boolean isPalindrome(int x) {
+        String in = String.valueOf(x);
+        String reversed = (new StringBuffer(in)).reverse().toString();
+        if (in.equals(reversed)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
