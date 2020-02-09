ac26d26111ca3f873baa22598381b32cf144c84d
anthonymin <dbstnsdl12@naver.com>
2020-02-09 22:44:36 +0900

    BARELY solved Tower problem from 2 platform
    Signed-off-by: anthonymin <dbstnsdl12@naver.com>

---
+++ BOJ/JAVA/Tower_2493.java
@@ -0,0 +1,72 @@
+package com.java.BOJ.JAVA;
+
+import java.io.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Tower_2493 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        int[] result = new int[N];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = N - 1; i >= 0; i--) {
+            int currentTop = stack.peek();
+            while (!stack.isEmpty() && arr[currentTop] < arr[i]) {
+                result[stack.pop()] = i + 1;
+            }
+            stack.push(i);
+        }
+
+        /*
+         *  반성을 위해 내가 직접 구현했던 코드 남겨둠.
+         *  현자타임...
+         */
+//        int k = N - 1;
+//        int idx = N - 1;
+//        int tmp = 0;
+//        boolean isFail = false;
+//        while (!stack.isEmpty()) {
+//            if (!isFail) {
+//                tmp = stack.pop();
+//            }
+//            if (!stack.isEmpty() && stack.peek() >= tmp) {
+//                arr[idx] = k;
+//                while (!stack1.isEmpty()) {
+//                    stack.push(stack1.pop());
+//                    k++;
+//                }
+//                idx--;
+//                isFail = false;
+//            } else {
+//                isFail = true;
+//                if (!stack.isEmpty())
+//                    stack1.push(stack.pop());
+//            }
+//            if (stack.isEmpty()) {
+//                arr[idx] = 0;
+//                idx--;
+//                k--;
+//                isFail = false;
+//                while (!stack1.isEmpty()) {
+//                    stack.push(stack1.pop());
+//                    k++;
+//                }
+//            }
+//            k--;
+//        }
+        for (int i = 0; i < N; i++) {
+            bw.append(String.valueOf(result[i])).append(" ");
+        }
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
---
+++ Programmers/Practice/Level2_Tower.java
@@ -0,0 +1,65 @@
+package com.java.Programmers.Practice;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class Level2_Tower {
+    public static void main(String[] args) {
+        int[] arr1 = {6, 9, 5, 7, 4};
+        System.out.println(Arrays.toString(solution(arr1)));
+        int[] arr2 = {3, 9, 9, 3, 5, 7, 2};
+        System.out.println(Arrays.toString(solution(arr2)));
+        int[] arr3 = {1, 5, 3, 6, 7, 6, 5};
+        System.out.println(Arrays.toString(solution(arr3)));
+        int[] arr4 = {6, 4, 5, 2, 5, 1};
+        System.out.println(Arrays.toString(solution(arr4)));
+    }
+
+    public static int[] solution(int[] heights) {
+        int N = heights.length;
+        int[] answer = new int[N];
+        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> stack1 = new Stack<>();
+        for (int i = 0; i < N; i++) {
+            stack.push(heights[i]);
+        }
+        int k = N - 1;
+        int idx = N - 1;
+        int tmp = 0;
+        boolean isFail = false;
+        while (!stack.isEmpty()) {
+            if (!isFail) {
+                tmp = stack.pop();
+            }
+            if (!stack.isEmpty() && stack.peek() >= tmp) {
+                answer[idx] = k;
+                while (!stack1.isEmpty()) {
+                    stack.push(stack1.pop());
+                    k++;
+                }
+                idx--;
+                isFail = false;
+            } else {
+                isFail = true;
+                if (!stack.isEmpty())
+                    stack1.push(stack.pop());
+            }
+//            System.out.println(stack1);
+//            System.out.println(stack);
+//            System.out.println(k);
+//            System.out.println(idx);
+            if (stack.isEmpty()) {
+                answer[idx] = 0;
+                idx--;
+                k--;
+                isFail = false;
+                while (!stack1.isEmpty()) {
+                    stack.push(stack1.pop());
+                    k++;
+                }
+            }
+            k--;
+        }
+        return answer;
+    }
+}
