a0fc2231769d83e68678fc9abc1c0374fcf0e43a
anthonymin <dbstnsdl12@naver.com>
2020-02-10 23:47:52 +0900

    solved 10866 Deque in BOJ, and refactored code in 10845
    Signed-off-by: anthonymin <dbstnsdl12@naver.com>

---
+++ BOJ/JAVA/Deque_10866.java
@@ -0,0 +1,64 @@
+package com.java.BOJ.JAVA;
+
+import java.io.*;
+import java.util.ArrayDeque;
+
+//리스트로 구현했을 때는 틀렸다고 뜸. 왜인지 모르겠음
+public class Deque_10866 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        for (int i = 0; i < N; i++) {
+            String[] inst = br.readLine().split(" ");
+            switch (inst[0]) {
+                case "push_back":
+                    deque.addLast(Integer.parseInt(inst[1]));
+                    break;
+                case "push_front":
+                    deque.addFirst(Integer.parseInt(inst[1]));
+                    break;
+                case "pop_front":
+                    if (deque.isEmpty())
+                        bw.append("-1\n");
+                    else {
+                        bw.append(String.valueOf(deque.removeFirst())).append("\n");
+                    }
+                    break;
+                case "pop_back":
+                    if (deque.isEmpty())
+                        bw.append("-1\n");
+                    else {
+                        bw.append(String.valueOf(deque.removeLast())).append("\n");
+                    }
+                    break;
+                case "size":
+                    bw.append(String.valueOf(deque.size())).append("\n");
+                    break;
+                case "empty":
+                    if (deque.isEmpty())
+                        bw.append("1\n");
+                    else
+                        bw.append("0\n");
+                    break;
+                case "front":
+                    if (deque.isEmpty())
+                        bw.append("-1\n");
+                    else
+                        bw.append(String.valueOf(deque.getFirst())).append("\n");
+                    break;
+                case "back":
+                    if (deque.isEmpty())
+                        bw.append("-1\n");
+                    else
+                        bw.append(String.valueOf(deque.getLast())).append("\n");
+                    break;
+            }
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
--- BOJ/JAVA/Queue_10845.java
+++ BOJ/JAVA/Queue_10845.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.util.LinkedList;
 import java.util.List;
 
+// 얘는 왜 리스트로 풀었는데도 통과되는걸까
 public class Queue_10845 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,32 +13,39 @@ public class Queue_10845 {
         List<Integer> list = new LinkedList<>();
         for (int i = 0; i < N; i++) {
             String[] inst = br.readLine().split(" ");
-            if (inst[0].equals("push")) {
-                list.add(Integer.parseInt(inst[1]));
-            } else if (inst[0].equals("pop")) {
-                if (list.isEmpty())
-                    bw.append("-1\n");
-                else {
-                    bw.append(list.get(0) + "\n");
-                    list.remove(list.get(0));
-                }
-            } else if (inst[0].equals("size")) {
-                bw.append(list.size() + "\n");
-            } else if (inst[0].equals("empty")) {
-                if (list.isEmpty())
-                    bw.append("1\n");
-                else
-                    bw.append("0\n");
-            } else if (inst[0].equals("front")) {
-                if (list.isEmpty())
-                    bw.append("-1\n");
-                else
-                    bw.append(list.get(0) + "\n");
-            } else if (inst[0].equals("back")) {
-                if (list.isEmpty())
-                    bw.append("-1\n");
-                else
-                    bw.append(list.get(list.size() - 1) + "\n");
+            switch (inst[0]) {
+                case "push":
+                    list.add(Integer.parseInt(inst[1]));
+                    break;
+                case "pop":
+                    if (list.isEmpty())
+                        bw.append("-1\n");
+                    else {
+                        bw.append(String.valueOf(list.get(0))).append("\n");
+                        list.remove(list.get(0));
+                    }
+                    break;
+                case "size":
+                    bw.append(String.valueOf(list.size())).append("\n");
+                    break;
+                case "empty":
+                    if (list.isEmpty())
+                        bw.append("1\n");
+                    else
+                        bw.append("0\n");
+                    break;
+                case "front":
+                    if (list.isEmpty())
+                        bw.append("-1\n");
+                    else
+                        bw.append(String.valueOf(list.get(0))).append("\n");
+                    break;
+                case "back":
+                    if (list.isEmpty())
+                        bw.append("-1\n");
+                    else
+                        bw.append(String.valueOf(list.get(list.size() - 1))).append("\n");
+                    break;
             }
         }
         bw.flush();
