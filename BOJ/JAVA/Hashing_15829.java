029e5c2ce0628f4da4a19501645694fe3e842490
anthonymin <dbstnsdl12@naver.com>
2020-02-11 23:06:53 +0900

    complete subtask 15829 Hashing in BOJ
    Signed-off-by: anthonymin <dbstnsdl12@naver.com>

--- BOJ/JAVA/Hashing_15829.java
+++ BOJ/JAVA/Hashing_15829.java
@@ -8,15 +8,20 @@ public class Hashing_15829 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        Map<Character, Integer> map = new LinkedHashMap<>();
+        Map<Character, Long> map = new LinkedHashMap<>();
         for (int i = 97; i <= 122; i++) {
-            map.put((char) i, i - 96);
+            map.put((char) i, (long) (i - 96));
         }
-        int n = Integer.parseInt(br.readLine());
+        long n = Long.parseLong(br.readLine());
         String a = br.readLine();
         long sum = 0;
+        long mul = 1;
+        long tmp = 0;
         for (int i = 0; i < n; i++) {
-            sum += Math.pow(31, i) * map.get(a.charAt(i));
+            sum %= 1234567891;
+            tmp = (map.get(a.charAt(i)) * mul) % 1234567891;
+            sum = (sum + tmp) % 1234567891;
+            mul = (mul * 31) % 1234567891;
         }
         System.out.println(sum);
     }
