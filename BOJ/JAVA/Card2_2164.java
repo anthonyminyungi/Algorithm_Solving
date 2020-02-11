d19ed2c55d7fd71200ef2844f163e0d5bd95ec79
anthonymin <dbstnsdl12@naver.com>
2020-02-11 22:20:59 +0900

    solved 2164 in BOJ
    Signed-off-by: anthonymin <dbstnsdl12@naver.com>

---
+++ BOJ/JAVA/Card2_2164.java
@@ -0,0 +1,28 @@
+package com.java.BOJ.JAVA;
+
+import java.io.*;
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Card2_2164 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        Deque<Integer> deque = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++) {
+            deque.addLast(i);
+        }
+        int i = 1;
+        while (deque.size() > 1) {
+            if (i % 2 == 0) {
+                deque.addLast(deque.removeFirst());
+            } else {
+                deque.removeFirst();
+            }
+            i++;
+        }
+        System.out.println(deque.getFirst());
+    }
+}
