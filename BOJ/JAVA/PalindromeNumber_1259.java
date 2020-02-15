a845668b2aac5308527b47caa9593d04a17a8c71
anthonymin <dbstnsdl12@naver.com>
2020-02-15 21:28:27 +0900

    Solved 2231, 17219, 1259, 5052 in BOJ, 1 Level2 problem in Programmers
    Signed-off-by: anthonymin <dbstnsdl12@naver.com>

---
+++ BOJ/JAVA/PalindromeNumber_1259.java
@@ -0,0 +1,29 @@
+package com.java.BOJ.JAVA;
+
+import java.io.*;
+
+public class PalindromeNumber_1259 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String in;
+        while (!(in = br.readLine()).equals("0")) {
+            boolean isPalindrome = true;
+            int len = in.length();
+            for (int i = 0; i <= len / 2; i++) {
+                System.out.println(in.charAt(i) + ", " + in.charAt(len - 1 - i));
+                if (in.charAt(i) != in.charAt(len - 1 - i)) {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+            if (!isPalindrome)
+                bw.append("no\n");
+            else
+                bw.append("yes\n");
+        }
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
