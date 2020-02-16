b3b6534fdcb2b50fb8673ba766bc0167c77915b1
anthonymin <dbstnsdl12@naver.com>
2020-02-16 17:15:29 +0900

    solved 2442, 2309 in BOJ
    Signed-off-by: anthonymin <dbstnsdl12@naver.com>

---
+++ BOJ/JAVA/SevenDwarfs_2309.java
@@ -0,0 +1,49 @@
+package com.java.BOJ.JAVA;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class SevenDwarfs_2309 {
+    static List<int[]> list = new LinkedList<>();
+
+    public static void main(String... args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int[] tall = new int[9];
+        int[] com = new int[9];
+        int[] answer = new int[7];
+        for (int i = 0; i < 9; i++) {
+            tall[i] = Integer.parseInt(br.readLine());
+        }
+        combination(com, 0, 9, 7, 0);
+        for (int[] ints : list) {
+            int sum = 0;
+            for (int i = 0; i < ints.length; i++) {
+                sum += tall[ints[i]];
+                answer[i] = tall[ints[i]];
+            }
+            if (sum == 100)
+                break;
+
+        }
+        Arrays.sort(answer);
+        for (int value : answer) {
+            System.out.println(value);
+        }
+    }
+
+    public static void combination(int[] arr, int index, int n, int r, int target) {
+        if (r == 0) {
+            int[] a = new int[index];
+            System.arraycopy(arr, 0, a, 0, index);
+            list.add(a);
+        } else if (target == n) return;
+        else {
+            arr[index] = target;
+            combination(arr, index + 1, n, r - 1, target + 1);
+            combination(arr, index, n, r, target + 1);
+        }
+    }
+}
---
+++ BOJ/JAVA/Start5_2442.java
@@ -0,0 +1,24 @@
+package com.java.BOJ.JAVA;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Start5_2442 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < 2 * N; j++) {
+                if (Math.abs(N - i - 1) <= j && Math.abs(N + i - 1) >= j)
+                    System.out.print("*");
+                else {
+                    if (Math.abs(N - i - 1) > j)
+                        System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
