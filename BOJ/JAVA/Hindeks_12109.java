1a5d9c4c45430e3adb81a8686960809844775054
anthonymin <dbstnsdl12@naver.com>
2020-02-16 15:29:51 +0900

    Solved H-index problem in BOJ(12109) and Programmers
    Signed-off-by: anthonymin <dbstnsdl12@naver.com>

---
+++ BOJ/JAVA/Hindeks_12109.java
@@ -0,0 +1,24 @@
+package com.java.BOJ.JAVA;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Hindeks_12109 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        String[] a = br.readLine().split(" ");
+        int[] arr = new int[n];
+        int[] h = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(a[i]);
+        }
+        Arrays.sort(arr);
+        for (int i = 0; i < n; i++) {
+            h[i] = Math.min(arr[i], n - i);
+        }
+        Arrays.sort(h);
+        System.out.println(h[n - 1]);
+    }
+}
---
+++ Programmers/Practice/Level2/Level2_HIndex.java
@@ -0,0 +1,25 @@
+package com.java.Programmers.Practice.Level2;
+
+import java.util.Arrays;
+
+public class Level2_HIndex {
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{3, 0, 6, 1, 5}));
+        System.out.println(solution(new int[]{20, 19, 18, 1}));
+        System.out.println(solution(new int[]{22, 42}));
+    }
+
+    public static int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);
+        System.out.println(Arrays.toString(citations));
+        int n = citations.length;
+        int[] h = new int[n];
+        for (int i = 0; i < n; i++) {
+            h[i] = Math.min(citations[i], citations.length - i);
+        }
+        Arrays.sort(h);
+        answer = h[n - 1];
+        return answer;
+    }
+}
