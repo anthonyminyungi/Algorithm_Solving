eaed712dce955ce828bcecf024d5d99e9ece2a1f
anthonymin <dbstnsdl12@naver.com>
2020-02-18 21:39:18 +0900

    solved 1929, 1699 in BOJ related to Prime number
    Signed-off-by: anthonymin <dbstnsdl12@naver.com>

---
+++ BOJ/JAVA/GetPrimeNumber_1929.java
@@ -0,0 +1,35 @@
+package com.java.BOJ.JAVA;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class GetPrimeNumber_1929 {
+    static boolean[] prime;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] in = br.readLine().split(" ");
+        int M = Integer.parseInt(in[0]);
+        int N = Integer.parseInt(in[1]);
+        prime = new boolean[N + 1];
+        prime[0] = prime[1] = true;
+
+        for (int i = 2; i * i <= N; i++) {
+            if (!prime[i]) {
+                for (int j = i * i; j <= N; j += i) {
+                    prime[j] = true;
+                    System.out.println(j);
+                }
+            }
+        }
+        for (int i = M; i <= N; i++) {
+            if (!prime[i]) bw.append(String.valueOf(i)).append("\n");
+        }
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
---
+++ BOJ/JAVA/SumOfSquares_1699.java
@@ -0,0 +1,28 @@
+package com.java.BOJ.JAVA;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SumOfSquares_1699 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[(int) Math.sqrt(n)];
+        int[] dp = new int[n + 1];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (i + 1) * (i + 1);
+        }
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            dp[i] = Integer.MAX_VALUE;
+        }
+
+        for (int value : arr) {
+            for (int j = value; j <= n; j++) {
+                dp[j] = Math.min(dp[j - value] + 1, dp[j]);
+            }
+        }
+        System.out.println(dp[n]);
+    }
+}
