a0726c12a6f3a083cdcb3e75a964489eceb31e8e
anthonymin <dbstnsdl12@naver.com>
2020-03-07 15:31:20 +0900

    solved Next Big Number problem in Programmers
    Signed-off-by: anthonymin <dbstnsdl12@naver.com>

---
+++ Programmers/Practice/Level2/Level2_NextBigNumber.java
@@ -0,0 +1,20 @@
+package com.java.Programmers.Practice.Level2;
+
+public class Level2_NextBigNumber {
+    public static void main(String[] args) {
+        System.out.println(solution(78));
+        System.out.println(solution(15));
+    }
+
+    public static int solution(int n) {
+        String bin = Integer.toBinaryString(n).replaceAll("0", "");
+        int answer = n + 1;
+        String a = "";
+        while (true) {
+            a = Integer.toBinaryString(answer).replaceAll("0", "");
+            if (a.equals(bin)) break;
+            else answer += 1;
+        }
+        return answer;
+    }
+}
