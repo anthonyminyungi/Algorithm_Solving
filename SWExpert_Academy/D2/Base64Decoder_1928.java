ed9a0c1e34d5509d5c5684977f11bfdcaed77c74
anthonymin <dbstnsdl12@naver.com>
2020-03-08 14:37:37 +0900

    solved 1928 problem in SWExpert Academy D2
    Signed-off-by: anthonymin <dbstnsdl12@naver.com>

---
+++ SWExpert_Academy/D2/Base64Decoder_1928.java
@@ -0,0 +1,26 @@
+package com.java.SWExpert_Academy.D2;
+
+
+import java.io.*;
+import java.util.Base64;
+
+public class Base64Decoder_1928 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        Base64.Decoder decoder = Base64.getDecoder();
+        for (int i = 1; i <= T; i++) {
+            String encodedString = br.readLine();
+            byte[] temp = decoder.decode(encodedString);
+            StringBuilder result = new StringBuilder();
+            for (int j = 0; j < temp.length; j++) {
+                result.append((char) temp[j]);
+            }
+            bw.append("#").append(String.valueOf(i)).append(" ").append(result.toString()).append("\n");
+        }
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
