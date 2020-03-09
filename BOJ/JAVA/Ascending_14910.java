34646bfcfeede6f0e0386180933c09388606a45e
anthonymin <dbstnsdl12@naver.com>
2020-03-09 21:19:31 +0900

    solved 14910, 14912 problem in BOJ
    Signed-off-by: anthonymin <dbstnsdl12@naver.com>

---
+++ BOJ/JAVA/Ascending_14910.java
@@ -0,0 +1,24 @@
+package com.java.BOJ.JAVA;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ascending_14910 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        boolean asc = true;
+        String[] in = br.readLine().split(" ");
+        int standard = Integer.MIN_VALUE;
+        for (String s : in) {
+            int cur = Integer.parseInt(s);
+            if (cur >= standard)
+                standard = cur;
+            else {
+                asc = false;
+                break;
+            }
+        }
+        System.out.println(asc ? "Good" : "Bad");
+    }
+}
---
+++ BOJ/JAVA/FrequentNumber_14912.java
@@ -0,0 +1,25 @@
+package com.java.BOJ.JAVA;
+
+import java.io.*;
+
+public class FrequentNumber_14912 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] in = br.readLine().split(" ");
+
+        int N = Integer.parseInt(in[0]);
+        int toFind = Integer.parseInt(in[1]);
+        int cnt = 0;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            sb.append(i);
+        }
+        char[] chars = sb.toString().toCharArray();
+        for (char aChar : chars) {
+            if (Integer.parseInt(String.valueOf(aChar)) == toFind)
+                cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
