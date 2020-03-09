8855f1999befd31867a1e3d4e7ea7f1d5cf41364
anthonymin <dbstnsdl12@naver.com>
2020-03-09 21:48:12 +0900

    solved 14918, 14909 problem in BOJ
    Signed-off-by: anthonymin <dbstnsdl12@naver.com>

---
+++ BOJ/JAVA/Plus_14918.java
@@ -0,0 +1,13 @@
+package com.java.BOJ.JAVA;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Plus_14918 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] arr = br.readLine().split(" ");
+        System.out.println(Integer.parseInt(arr[0]) + Integer.parseInt(arr[1]));
+    }
+}
---
+++ BOJ/JAVA/PositiveNumber_14909.java
@@ -0,0 +1,18 @@
+package com.java.BOJ.JAVA;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class PositiveNumber_14909 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cnt = 0;
+        String[] arr = br.readLine().split(" ");
+        for (String s : arr) {
+            if (Integer.parseInt(s) > 0)
+                cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
