312da7e116752e5080cd0bcbf9ad99fc92a8112f
anthonyminyungi <dbstnsdl12@naver.com>
2020-04-08 19:44:35 +0900

    Solved CorrectBracket problem in Programmers Level2
    Signed-off-by: anthonyminyungi <dbstnsdl12@naver.com>

--- Programmers/Kakao2019InternTest/Solution4.java
+++ Programmers/Kakao2019InternTest/Solution4.java
@@ -25,7 +25,6 @@ public class Solution4 {
                         break;
                     }
                 }
-
             }
         }
 
---
+++ Programmers/Practice/Level2/Level2_CorrectBracket.java
@@ -0,0 +1,24 @@
+package com.java.Programmers.Practice.Level2;
+
+public class Level2_CorrectBracket {
+    public static void main(String[] args) {
+        System.out.println(solution("()()"));
+        System.out.println(solution("(())()"));
+        System.out.println(solution(")()("));
+        System.out.println(solution("(()("));
+    }
+
+    public static boolean solution(String s) {
+        int det = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(')
+                det += 1;
+            else {
+                if (det == 0)
+                    return false;
+                det -= 1;
+            }
+        }
+        return det == 0;
+    }
+}
