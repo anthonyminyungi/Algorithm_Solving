007a779da361d277c47102464f450dce097a02f8
anthonyminyungi <dbstnsdl12@naver.com>
2020-04-11 16:52:53 +0900

    Solved NumberExpression problem in Programmers Level2
    Signed-off-by: anthonyminyungi <dbstnsdl12@naver.com>

---
+++ Programmers/Practice/Level2/Level2_NumberExpression.java
@@ -0,0 +1,31 @@
+package com.java.Programmers.Practice.Level2;
+
+public class Level2_NumberExpression {
+    public static void main(String[] args) {
+        System.out.println(solution(6));
+        System.out.println(solution(9));
+        System.out.println(solution(8));
+        System.out.println(solution(1800));
+        System.out.println(solution(15));
+        System.out.println(solution(10000));
+    }
+
+    public static int solution(int n) {
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            if (correct(i, n))
+                answer++;
+        }
+        return answer;
+    }
+
+    public static boolean correct(int k, int n) {
+        int sum = 0;
+        for (int i = k; i <= n; i++) {
+            sum += i;
+            if (sum >= n)
+                break;
+        }
+        return sum == n;
+    }
+}
