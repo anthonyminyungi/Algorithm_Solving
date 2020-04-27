85cedc67aa7c143e4f35aff47bf6490d740ff78b
anthonyminyungi <dbstnsdl12@naver.com>
2020-04-27 15:41:48 +0900

    Solved FindingPrimeNumber problem in Programmers Level2
    Signed-off-by: anthonyminyungi <dbstnsdl12@naver.com>

---
+++ Programmers/Practice/Level2/Level2_FindingPrimeNumber.java
@@ -0,0 +1,37 @@
+package com.java.Programmers.Practice.Level2;
+
+public class Level2_FindingPrimeNumber {
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{1, 2, 3, 4}));
+        System.out.println(solution(new int[]{1, 2, 7, 6, 4}));
+    }
+
+    public static int solution(int[] nums) {
+        int answer = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j != i && j < nums.length; j++) {
+                for (int k = 0; k != i && k != j && k < nums.length; k++) {
+                    int a = nums[i] + nums[j] + nums[k];
+                    if (isPrime(a))
+                        answer++;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static boolean isPrime(int k) {
+        boolean isPrime = false;
+        if (k != 1 && k != 999 && k != 1000) {
+            isPrime = true;
+
+            for (int j = 2; j < k; j++) {
+                if (k % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+        return isPrime;
+    }
+}
