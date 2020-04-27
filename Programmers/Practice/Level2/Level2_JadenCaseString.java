8097870a57601e8c2a6a99d5ac3d5f2e2b9bed29
anthonyminyungi <dbstnsdl12@naver.com>
2020-04-27 21:42:16 +0900

    Solved JadenCaseString problem in Programmers Level2
    Signed-off-by: anthonyminyungi <dbstnsdl12@naver.com>

---
+++ Programmers/Practice/Level2/Level2_JadenCaseString.java
@@ -0,0 +1,28 @@
+package com.java.Programmers.Practice.Level2;
+
+public class Level2_JadenCaseString {
+    public static void main(String[] args) {
+        System.out.println(solution("3people unFollowed me"));
+        System.out.println(solution("for the last week"));
+    }
+
+    public static String solution(String s) {
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (i == 0 || s.charAt(i - 1) == ' ') {
+                if (Character.isLetter(s.charAt(i))) {
+                    answer.append(String.valueOf(s.charAt(i)).toUpperCase());
+                } else {
+                    answer.append(s.charAt(i));
+                }
+            } else {
+                if (Character.isLetter(s.charAt(i))) {
+                    answer.append(String.valueOf(s.charAt(i)).toLowerCase());
+                } else {
+                    answer.append(s.charAt(i));
+                }
+            }
+        }
+        return answer.toString();
+    }
+}
