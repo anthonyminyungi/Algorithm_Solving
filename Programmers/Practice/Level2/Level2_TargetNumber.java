7458b53a00a3f1eb4cb90b3c5cf09c78447fd58a
anthonyminyungi <dbstnsdl12@naver.com>
2020-04-27 21:55:34 +0900

    Solved TargetNumber problem in Programmers Level2
    Signed-off-by: anthonyminyungi <dbstnsdl12@naver.com>

---
+++ Programmers/Practice/Level2/Level2_TargetNumber.java
@@ -0,0 +1,27 @@
+package com.java.Programmers.Practice.Level2;
+
+public class Level2_TargetNumber {
+    static int result = 0;
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{1, 1, 1, 1, 1}, 3));
+    }
+
+    public static int solution(int[] numbers, int target) {
+        int answer = 0;
+        dfs(numbers, target, 0, 0);
+        answer = result;
+        return answer;
+    }
+
+    public static void dfs(int[] numbers, int target, int index, int current) {
+        if (index == numbers.length) {
+            if (current == target) {
+                result++;
+            }
+            return;
+        }
+        dfs(numbers, target, index + 1, current + numbers[index]);
+        dfs(numbers, target, index + 1, current - numbers[index]);
+    }
+}
