3d10ed54bfb8e70de3a8001708a6aaf32f2302d5
anthonyminyungi <dbstnsdl12@naver.com>
2020-05-23 20:05:29 +0900

    Solved Summer Coding problems in Programmers
    Signed-off-by: anthonyminyungi <dbstnsdl12@naver.com>

---
+++ Programmers/SummerCoding/Programming1.java
@@ -0,0 +1,33 @@
+package com.java.Programmers.SummerCoding;
+
+public class Programming1 {
+    public static void main(String[] args) {
+        System.out.println(solution(1987));
+        System.out.println(solution(2015));
+    }
+
+    public static int solution(int p) {
+        int answer = 0;
+        int a = p;
+        while (true) {
+            a++;
+            boolean isNotBeautiful = false;
+            char[] c = String.valueOf(a).toCharArray();
+            for (int i = 0; i < c.length; i++) {
+                for (int j = 0; j < c.length; j++) {
+                    if (c[i] == c[j] && i != j) {
+                        isNotBeautiful = true;
+                        break;
+                    }
+                }
+                if (isNotBeautiful)
+                    break;
+            }
+            if (!isNotBeautiful) {
+                answer = Integer.parseInt(String.valueOf(c));
+                break;
+            }
+        }
+        return answer;
+    }
+}
---
+++ Programmers/SummerCoding/Programming2.java
@@ -0,0 +1,41 @@
+package com.java.Programmers.SummerCoding;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Programming2 {
+    public static void main(String[] args) {
+        System.out.println(solution(4));
+        System.out.println(solution(11));
+    }
+
+    //3의 20승까지
+    public static long solution(long n) {
+        long answer = 0;
+        long[] tempList = new long[21];
+        tempList[0] = 1;
+        tempList[1] = 3;
+        tempList[2] = 9;
+        List<Long> list = new ArrayList<>();
+        list.add((long) 1);
+        list.add((long) 3);
+        list.add((long) 4);
+        list.add((long) 9);
+        list.add((long) 10);
+        list.add((long) 12);
+        list.add((long) 13);
+
+        for (int i = 3; i <= 20; i++) {
+            long current = (long) Math.pow(3, i);
+            int listSize = list.size();
+            list.add(current);
+            for (int j = 0; j < listSize; j++) {
+                list.add(current + list.get(j));
+            }
+        }
+        Collections.sort(list);
+        answer = list.get((int) n - 1);
+        return answer;
+    }
+}
