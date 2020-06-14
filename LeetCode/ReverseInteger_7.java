1fb0a52bd258e87ab00d40a95c244f96f62c60ef
anthonyminyungi <dbstnsdl12@naver.com>
2020-06-15 00:17:54 +0900

    Solved ReverseInteger problem in LeetCode No.7(Easy)
    Signed-off-by: anthonyminyungi <dbstnsdl12@naver.com>

---
+++ LeetCode/ReverseInteger_7.java
@@ -0,0 +1,20 @@
+package com.java.LeetCode;
+
+// Easy
+public class ReverseInteger_7 {
+    public static void main(String[] args) {
+        System.out.println(reverse(123));
+        System.out.println(reverse(-321));
+        System.out.println(reverse(120));
+    }
+
+    // Reverse "Integer" 라고 했으면서 입력으로 9646324351 넣는 문제 수준...
+    public static int reverse(int x) {
+        String res = x < 0 ? new StringBuilder(String.valueOf(Math.abs(x))).append("-").reverse().toString() : new StringBuilder(String.valueOf(Math.abs(x))).reverse().toString();
+        try {
+            return Integer.parseInt(res);
+        } catch (Exception e) {
+            return 0;
+        }
+    }
+}
