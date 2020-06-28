9402c6e738001a2bf9b3671b99a08e13d87dc86d
anthonyminyungi <dbstnsdl12@naver.com>
2020-06-28 22:11:11 +0900

    Solved RemoveWithPair problem in Programmers Level2
    Signed-off-by: anthonyminyungi <dbstnsdl12@naver.com>

---
+++ Programmers/Practice/Level2/Level2_RemoveWithPair.java
@@ -0,0 +1,31 @@
+package com.java.Programmers.Practice.Level2;
+
+import java.util.Stack;
+
+public class Level2_RemoveWithPair {
+    public static void main(String[] args) {
+        System.out.println(solution("baabaa"));
+        System.out.println(solution("cdcd"));
+        System.out.println(solution("abccaeeaba"));
+        System.out.println(solution("aaaaaaaaaaaaa"));
+    }
+
+    public static int solution(String s) {
+        int answer = 0;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (stack.isEmpty())
+                stack.push(s.charAt(i));
+            else {
+                if (stack.peek() == s.charAt(i)) {
+                    stack.pop();
+                } else {
+                    stack.push(s.charAt(i));
+                }
+            }
+        }
+        if (stack.isEmpty())
+            answer = 1;
+        return answer;
+    }
+}
