ab39b82151cebff5c43e1dba877c4239f19f53ef
anthonyminyungi <dbstnsdl12@naver.com>
2020-07-05 22:58:39 +0900

    Modified SmartTrain:re-scored problem in BOJ 2455
    Signed-off-by: anthonyminyungi <dbstnsdl12@naver.com>

--- BOJ/JAVA/SmartTrain_2455.java
+++ BOJ/JAVA/SmartTrain_2455.java
@@ -7,14 +7,16 @@ public class SmartTrain_2455 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int passenger = 0;
+        int current = 0;
         for (int i = 0; i < 4; i++) {
             String a = br.readLine();
             String[] aa = a.split(" ");
             int out = Integer.parseInt(aa[0]);
             int in = Integer.parseInt(aa[1]);
-            passenger = passenger > passenger - out + in ? passenger : passenger - out + in;
+            current = current - out + in;
+            passenger = Math.max(passenger, current);
         }
-        bw.append("" + passenger);
+        bw.append("").append(String.valueOf(passenger));
         bw.flush();
         bw.close();
         br.close();
