b49a8823452d7c1da85460bf96ce408be223285a
anthonyminyungi <dbstnsdl12@naver.com>
2020-10-09 00:38:42 +0900

    Solved Problem 1,2 from Monthly Code Challenge
    Signed-off-by: anthonyminyungi <dbstnsdl12@naver.com>

---
+++ Programmers/MonthlyCodeChallenge/Oct/Programming1.java
@@ -0,0 +1,23 @@
+package com.algorithms.Programmers.MonthlyCodeChallenge.Oct;
+
+public class Programming1 {
+    public static void main(String[] args) {
+        System.out.println(solution(45));
+        System.out.println(solution(125));
+    }
+
+    public static int solution(int n) {
+        int answer = 0;
+        String third = Integer.toString(n, 3);
+        StringBuffer sb = new StringBuffer(third);
+        String reversed = sb.reverse().toString();
+
+        int exp = 0;
+        for (int i = reversed.length() - 1; i >= 0; i--) {
+            answer += Integer.parseInt(String.valueOf(reversed.charAt(i))) * Math.pow(3, exp);
+            exp++;
+        }
+
+        return answer;
+    }
+}
---
+++ Programmers/MonthlyCodeChallenge/Oct/Programming2.java
@@ -0,0 +1,50 @@
+package com.algorithms.Programmers.MonthlyCodeChallenge.Oct;
+
+import java.util.*;
+
+public class Programming2 {
+    static int[] answer;
+    private static int m;
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(new int[][]{{1, 1, 0, 0}, {1, 0, 0, 0}, {1, 0, 0, 1}, {1, 1, 1, 1}})));
+    }
+
+    public static int[] solution(int[][] arr) {
+        answer = new int[2];
+        int n = arr.length;
+
+        divide(0, 0, n, arr);
+
+        return answer;
+    }
+
+    private static boolean check(int row, int col, int n, int[][] map) {
+        int std = map[row][col];
+        for (int i = row; i < row + n; i++) {
+            for (int j = col; j < col + n; j++) {
+                if (std != map[i][j]) {
+                    return false;
+                }
+            }
+        }
+        m = std;
+        return true;
+    }
+
+    private static void divide(int row, int col, int n, int[][] map) {
+        if (check(row, col, n, map)) {
+            if (m == 0)
+                answer[0]++;
+            else
+                answer[1]++;
+        } else {
+            int s = n / 2;
+            for (int i = 0; i < 2; i++) {
+                for (int j = 0; j < 2; j++) {
+                    divide(row + s * i, col + s * j, s, map);
+                }
+            }
+        }
+    }
+}
