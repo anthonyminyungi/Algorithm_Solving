2c26ad3e7721a5a0ccb8ef44bca7df3e25bc7ff3
anthonyminyungi <dbstnsdl12@naver.com>
2020-10-19 14:58:25 +0900

    Modified NumSort problem from BOJ (재채점)
    Signed-off-by: anthonyminyungi <dbstnsdl12@naver.com>

--- BOJ/JAVA/NumSort_2751.java
+++ BOJ/JAVA/NumSort_2751.java
@@ -1,34 +1,26 @@
 package com.algorithms.BOJ.JAVA;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Iterator;
+import java.io.*;
+import java.util.*;
 
 public class NumSort_2751 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        HashSet<Integer> hashSet = new HashSet<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         for (int i = 0; i < N; i++) {
-            hashSet.add(Integer.parseInt(br.readLine()));
+            pq.add(Integer.parseInt(br.readLine()));
         }
-//        System.out.println(hashSet);
-        int[] arr = new int[hashSet.size()];
 
-        int i = 0;
-        Iterator iterator = hashSet.iterator();
-        while (iterator.hasNext()) {
-            arr[i] = (int) iterator.next();
-            i++;
+        StringBuilder sb = new StringBuilder();
+        while (!pq.isEmpty()) {
+            sb.append(pq.poll()).append("\n");
         }
-        Arrays.sort(arr);
-        for (int j = 0; j < arr.length; j++) {
-            System.out.println(arr[j]);
-        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
