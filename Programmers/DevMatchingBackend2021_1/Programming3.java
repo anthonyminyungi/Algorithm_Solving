2f5f4da13b542cdf279e856227334c34cb92afcc
anthonyminyungi <dbstnsdl12@naver.com>
2021-05-01 20:20:45 +0900

    Solved again Backend DevMatching problem from Programmers
    Signed-off-by: anthonyminyungi <dbstnsdl12@naver.com>

--- Programmers/DevMatchingBackend2021_1/Programming3.java
+++ Programmers/DevMatchingBackend2021_1/Programming3.java
@@ -3,6 +3,7 @@ package com.algorithms.Programmers.DevMatchingBackend2021_1;
 import java.util.*;
 
 // 거의 다풀었는데 시간 부족으로 미제출
+// 5.1 기존 푼 코드로 실패떠서 다시품
 public class Programming3 {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(solution(new String[]{"john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"},
@@ -16,59 +17,45 @@ public class Programming3 {
     }
 
     public static int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
-        int[] answer = {};
+        int[] answer = new int[enroll.length];
 
-        Node root = new Node("center", 0);
 
-        Node[] tree = new Node[enroll.length + 1];
-        tree[0] = root;
+        Map<String, Seller> sellerMap = new HashMap<>();
 
-        Map<String, Integer> idxMap = new HashMap<>();
-        Map<String, String> parentMap = new HashMap<>();
-        idxMap.put("center", 0);
-        for (int i = 0; i < enroll.length; i++) {
-            tree[i + 1] = new Node(enroll[i], 0);
-            idxMap.put(enroll[i], i + 1);
-            if (referral[i].equals("-")) {
-                tree[0].childList.add(tree[i + 1]);
-                parentMap.put(enroll[i], "center");
-            } else {
-                tree[idxMap.get(referral[i])].childList.add(tree[i + 1]);
-                parentMap.put(enroll[i], referral[i]);
-            }
-        }
+        sellerMap.put("-", new Seller("-"));
+        for (String e : enroll)
+            sellerMap.put(e, new Seller(e));
 
-        for (int i = 0; i < seller.length; i++) {
-            String name = seller[i];
-            int sales = 100 * amount[i];
-            while (parentMap.containsKey(name)) {
-                String ref = parentMap.get(name);
-                int upper = (int) Math.round((double) sales / 10);
-                Node current = tree[idxMap.get(name)];
-                current.earn += (sales - upper);
+        for (int i = 0; i < referral.length; i++)
+            sellerMap.get(enroll[i]).parent = sellerMap.get(referral[i]);
 
-                name = ref;
-                sales = upper;
-            }
-        }
+        for (int i = 0; i < seller.length; i++)
+            dfs(sellerMap.get(seller[i]), amount[i] * 100);
 
-        answer = new int[enroll.length];
+        for (int i = 0; i < enroll.length; i++)
+            answer[i] = sellerMap.get(enroll[i]).profit;
 
-        for (int i = 0; i < enroll.length; i++) {
-            answer[i] = tree[i + 1].earn;
-        }
         return answer;
     }
 
-    static class Node {
-        String name;
-        int earn;
-        List<Node> childList;
+    static void dfs(Seller seller, int profit) {
+        int parentProfit = profit / 10;
+        if (parentProfit != 0 && seller.parent != null) {
+            seller.profit += (profit - parentProfit);
+            dfs(seller.parent, parentProfit);
+        } else
+            seller.profit += profit;
+    }
 
-        Node(String n, int e) {
+    static class Seller {
+        String name;
+        int profit;
+        Seller parent;
+
+        Seller(String n) {
             this.name = n;
-            this.earn = e;
-            this.childList = new ArrayList<>();
+            this.profit = 0;
+            this.parent = null;
         }
     }
 }
