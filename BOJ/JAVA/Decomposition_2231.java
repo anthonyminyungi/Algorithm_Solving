af0eccec2e7ddb04e83ead41be0e2b708cd56e7d
anthonyminyungi <dbstnsdl12@naver.com>
2021-08-12 21:03:45 +0900

    solved again 2331 problem from BOJ
    Signed-off-by: anthonyminyungi <dbstnsdl12@naver.com>

--- BOJ/JAVA/Decomposition_2231.java
+++ BOJ/JAVA/Decomposition_2231.java
@@ -8,27 +8,20 @@ public class Decomposition_2231 {
 
         String in = br.readLine();
         int n = Integer.parseInt(in);
-        int tmp = n;
-        if (tmp > 9) tmp -= in.length() * 9;
-        if (tmp < 0) tmp = 0;
 
-        boolean have = false;
-        int answer = 0;
-        for (int i = tmp; i < n; i++) {
-            int sum = i;
-            String traversal = String.valueOf(i);
-            for (int j = 0; j < traversal.length(); j++) {
-                sum += Integer.parseInt(String.valueOf(traversal.charAt(j)));
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum = i;
+            String temp = String.valueOf(i);
+            for (int j = 0; j < temp.length(); j++) {
+                sum += (temp.charAt(j) - '0');
             }
             if (sum == n) {
-                have = true;
-                answer = i;
-                break;
+                System.out.println(i);
+                return;
             }
         }
-
-        if (!have) answer = 0;
-
-        System.out.println(answer);
+        System.out.println(0);
     }
 }
